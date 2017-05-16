7d99e50e8607be8fab54fe0de301bf4c5f51ddcf
Justina Li <justinali@Justinas-MacBook-Pro.local>
2017-05-16 14:31:50 +0800

    Get links from midland pages

--- src/propertyList.js
+++ src/propertyList.js
@@ -1,28 +1,45 @@
 const Nightmare = require('nightmare');
 
-function getListingLinks () {
-  const nightmare = Nightmare({
-    show: true,
-  });
-
+function getLinksPerPage(nightmare) {
   return nightmare
-    .goto('http://en.midland.com.hk/find-property/#list')
     .evaluate(() => {
       const urls = $('#tpl-thumb a')
-        .map((idx, elem) => elem.href).get();
-      return {
-        urls,
-      }
+        .map((idx, elem) => {
+          let type = 'rent';
+          let $elem = $(elem);
+          if ($elem.find('span.sell').length && $elem.find('span.rent').length) {
+            type = 'both';
+          } else if ($elem.find('span.sell').length) {
+            type = 'buy';
+          }
+          return { type, url: elem.href };
+        }).get();
+
+      return { urls, nextPageDisabled: $('#pagination :not(.prev).disabled + li').hasClass('disabled') };
     })
-    .end()
-    .then((result) => {
-      console.log('result', result);
-      return result;
+    // .end()
+    .then(({ urls, nextPageDisabled }) => {
+      console.log('urls', urls);
+      // return result;
+      if (!nextPageDisabled) {
+        return nightmare.wait(2000).click('#pagination :not(.prev).disabled + li')
+          .then(() => getLinksPerPage(nightmare));
+      }
+
+      return nightmare.end();
     })
     .catch((error) => {
       console.error('error happened' , error);
     });
-    console.log('result', result);
+}
+
+function getListingLinks () {
+  const nightmare = Nightmare({
+    show: true,
+  });
+  return nightmare
+    .goto('http://en.midland.com.hk/find-property/#list')
+    .then(() => getLinksPerPage(nightmare));
 }
 
 getListingLinks();
